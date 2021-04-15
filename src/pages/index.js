816389b49a0e2787d75ef1d80bca5ce23b99664c
Cristian1t <cristianturmacu@yahoo.com>
2021-04-15 23:01:11 +0200

    index refactor

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,45 +1,6 @@
-import { StaticImage } from "gatsby-plugin-image"
 import React from "react"
-import Hamburger from "../components/Hamburger"
-import styled from "styled-components"
-
-export const StyledIndex = styled.div`
-  .heroimg {
-    opacity: 40%;
-  }
-  .herowrap {
-    z-index: 1;
-    background: black;
-    .herotxt {
-      position: absolute;
-      top: 20%;
-      text-align: center;
-      width: 100%;
-      color: ${({ theme }) => theme.color.background};
-      font-family: ${({ theme }) => theme.fonts.header};
-      font-size: 2rem;
-    }
-  }
-`
+import Hero from "../components/Hero"
 
 export default function Home() {
-  return (
-    <StyledIndex>
-      <div className="herowrap">
-        <StaticImage
-          className="heroimg"
-          src="../images/hero2.jpg"
-          alt="Mosta Saloon Hero"
-          placeholder="blurred"
-          layout="fullWidth"
-        />
-        <div className="herotxt">
-          <h1 className="">MOSTA SALOON</h1>
-          <p className="herop">389 420 7885</p>
-        </div>
-      </div>
-
-      <Hamburger />
-    </StyledIndex>
-  )
+  return <Hero />
 }
